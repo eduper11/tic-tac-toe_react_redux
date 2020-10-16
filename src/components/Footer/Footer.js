213443b557288f4fe0187859c6e3b742123d4d8b
Edu Pérez <eduper11@gmail.com>
2020-10-16 14:49:37 +0200

    github url change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className='footer'>
             <a
                 className='footer__link'
-                href='https://github.com/eduper11'
+                href='https://github.com/eduper11/tic-tac-toe_react_redux'
                 target='_blank'
                 rel='noreferrer noopener'
             >
